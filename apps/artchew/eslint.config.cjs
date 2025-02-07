fdb0344abf0686f005c8ec1ca403994a5a35678b
Kha Mai <>
2025-02-07 21:14:13 +0700

    chore: remove un-use

--- apps/artchew/eslint.config.cjs
+++ apps/artchew/eslint.config.cjs
@@ -1,6 +1,5 @@
 const { FlatCompat } = require('@eslint/eslintrc');
 const js = require('@eslint/js');
-const { fixupConfigRules } = require('@eslint/compat');
 const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.cjs');
 
@@ -10,9 +9,9 @@ const compat = new FlatCompat({
 });
 
 module.exports = [
-  ...fixupConfigRules(compat.extends('next')),
+  ...compat.extends('next'),
 
-  ...fixupConfigRules(compat.extends('next/core-web-vitals')),
+  ...compat.extends('next/core-web-vitals'),
 
   ...baseConfig,
   ...nx.configs['flat/react-typescript'],
