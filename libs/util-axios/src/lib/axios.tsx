e3bcaf32d2f2f15a8c578bdf2eb257246f8d449c
Kha Mai <>
2025-03-08 14:03:07 +0700

    chore: use base URL

--- libs/util-axios/src/lib/axios.tsx
+++ libs/util-axios/src/lib/axios.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// http://localhost:3000
+const BASE_URL = 'http://zow8kk8ooosk4kso84ggkckg.89.116.23.169.sslip.io'
+
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: BASE_URL,
 });
 
 export default instance;
