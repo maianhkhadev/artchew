10516e034b8312e6ee5b71bbec580702265654b2
Kha Mai <>
2025-03-08 20:27:47 +0700

    chore: fix build issue

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router';
-import App from './app/App';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { PrivateRoute } from './app/PrivateRoute';
+import { Welcome } from './app/pages/Welcome';
+import { StudentList } from './app/pages/Students/List';
+import { StudentEdit } from './app/pages/Students/Edit';
 
 const queryClient = new QueryClient();
 
@@ -14,7 +17,14 @@ root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route index element={<Welcome />} />
+
+          <Route element={<PrivateRoute />}>
+            <Route path="students" element={<StudentList />} />
+            <Route path="student/:id" element={<StudentEdit />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
