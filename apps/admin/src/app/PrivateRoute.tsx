f3786fd8ef899ed85bf5be830580ba2fc24a91c2
Kha Mai <>
2025-03-09 00:09:05 +0700

    chore: add private router

--- apps/admin/src/app/PrivateRoute.tsx
+++ apps/admin/src/app/PrivateRoute.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router';
+import { Navigate, Outlet } from 'react-router';
+
+const token = sessionStorage.getItem('token');
 
 export function PrivateRoute() {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  if (!token) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
 }
 
 export default PrivateRoute;
--- apps/admin/src/app/pages/Welcome/Welcome.tsx
+++ apps/admin/src/app/pages/Welcome/Welcome.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Title } from 'rebear';
 import { FormLogin } from '../../components/FormLogin';
 import styles from './Welcome.module.scss';
 
 export function Welcome() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSubmit = (values: any) => {}
+  const handleSubmit = (values: any) => {
+    const { email, password } = values;
+    if (email === 'admin@artchew.com' && password === 'admin123') {
+      sessionStorage.setItem('token', 'artchew_token');
+      navigate('/students');
+    }
+  };
 
   return (
     <div className={styles.page}>
-      <FormLogin isLoading={isLoading} onSubmit={handleSubmit} />
+      <header className={styles.header}>
+        <Title level={2}>Artchew Admin</Title>
+      </header>
+      <main className={styles.main}>
+        <FormLogin isLoading={isLoading} onSubmit={handleSubmit} />
+      </main>
     </div>
   );
 }
