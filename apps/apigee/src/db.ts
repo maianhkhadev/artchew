f884c167d5d891a966c55f56ecf226a30e62773e
Kha Mai <>
2025-03-19 21:54:37 +0700

    chore: update

--- apps/apigee/src/db.ts
+++ apps/apigee/src/db.ts
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
-const DATABASE_CONNECTION = 'mongodb://root:jUTNy5NQno898cQgRzDXL5Z4Wwr3CCv13keuL8k3enwli4WvIqHx5RleM69TFVOM@89.116.23.169:5432/?directConnection=true'
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(DATABASE_CONNECTION);
+    await mongoose.connect(process.env.DATABASE);
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
--- libs/util-axios/src/lib/axios.tsx
+++ libs/util-axios/src/lib/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // http://localhost:3000
-const BASE_URL = 'http://zow8kk8ooosk4kso84ggkckg.89.116.23.169.sslip.io'
+const BASE_URL = 'http://g0oosc84s0sgwwk0o000gso0.109.106.255.129.sslip.io'
 
 const instance = axios.create({
   baseURL: BASE_URL,
